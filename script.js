c684b02c277285b865811b5b3e370d61740da3e9
anash5119 <anash5119@gmail.com>
2023-07-10 12:47:14 -0700

    Fix calculator log, add display functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let num1;
-let num2; 
-let operator;
+let num1 = "";
+let num2 = ""; 
+let operator = "";
+let chaining = false; //used to show if you are chaining operations
+
+const displayContent = document.querySelector(".display");
 
 
 function add(firstNum, secondNum) {
@@ -13,6 +16,7 @@ function subtract(firstNum, secondNum) {
 
 function multiply(...numbers) {
   let product = 1;
+  console.log(numbers);
   for (const number of numbers){
     product = product * number;
   }
@@ -27,47 +31,85 @@ function operate(){
     if(num1 == undefined || num2 == undefined || operator == undefined){return}
     if(operator == "+"){
         display(add(num1, num2));
+        chaining = true; 
         num1 = add(num1, num2);
-    }
+        console.log("after equals:" + num1);
+        }
     else if(operator == "-"){
         display(subtract(num1, num2));
-        num1 = add(num1, num2);
+        num1 = subtract(num1, num2);
+        console.log("after equals:" + num1);
     }
     else if(operator == "*"){
         display(multiply(num1, num2));
-        num1 = add(num1, num2);
+        num1 = multiply(num1, num2);
+        console.log("after equals:" + num1);
     }
     else if(operator == "/"){
-        display(divide(num1, num2));
-        num1 = add(num1, num2);
+        if(num2 == 0){
+            displayContent.textContent = "You can't divide by 0 you donut"
+        }
+        else{
+            display(divide(num1, num2));
+            num1 = divide(num1, num2);
+            console.log("after equals:" + num1);
+        }
+        
     }
+    num2 = "";
+    chaining = true; 
 }
 
 function display(id){
-    const display = document.querySelector(".display");
+    //const display = document.querySelector(".display");
 
     if(id == "clear"){
         num1 = "";
         num2 = "";
         operator = "";
-        display.textContent = "";
+        chaining = false; 
+        console.log("after clear:" + num1 + num2 + operator);
+        displayContent.textContent = "";
     }
     else{
-        display.textContent = id;
+        displayContent.textContent = id;
     }
 }
 
 function storeNum(id){
-    if(num1 == undefined){
-        num1 = id;
+    if(!chaining && operator == ""){
+        num1 += id;
+        display(num1);
+    }
+    else if(chaining){
+        display("clear");
+        storeNum(id);
     }
     else{
-        num2 = id;
+        num2 += id;
+        display(num2);
     }
-    display(id);
+
+    // if(num1 == undefined || num1 == ""){
+    //     num1 = id;
+    //     console.log(num1);
+    // }
+    // else{
+    //     num2 += id;
+    //     console.log(num2);
+    // }
+    //display(id);
 }
 
 function storeOperator(id){
-    operator = id;
+    if(operator != undefined || operator != ""){
+        operate();
+        operator = id;
+    }
+    else{
+        operator = id;
+        console.log(operator);
+    }
+    chaining = false;
     display(id);
 }
